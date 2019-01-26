7fe1e2f8d5ebcf735312008e811b0cfc37389367
Santeri Honkanen <santeri.honkanen@nolwenture.com>
2019-01-26 18:30:12 +0200

    Lua Interpreter

---
+++ Assets/Scripts/LuaInterpreter/ControlProxy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoonSharp.Interpreter;
+
+public class ScriptReader
+{
+    public List<float> ForcesValues(string script, float[] forces)
+    {
+        List<float> vals = new List<float>();
+        Script s = new Script();
+        s.Globals["numbers"] = forces;
+        try
+        {
+            DynValue res = s.DoString(script);
+            if (res.Tuple != null)
+                for (int i = 0; i < res.Tuple.Length; i++)
+                {
+                    vals.Add((float)res.Tuple[i].Number);
+                }   
+        } catch (ScriptRuntimeException ex)
+        {
+            Debug.Log(ex);
+        }
+        return vals.Count > 0 ? vals : new List<float> { 2, 2, 2, 2 };
+    }
+}
